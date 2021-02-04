de220db9840eeafdc6966d42092f4c88764d54a1
unknown <flucasjs@gmail.com>
2021-02-03 23:48:04 -0800

    updated solution

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,27 +1,8 @@
 export const isPangram = (pangram) => {
-    if (!pangram) return false;
-
-    const alphabet = new (function Alphabet() {
-        for (const char of "abcdefghijklmnopqrstuvwxyz") {
-            this[`${char}`] = 0;
-        }
-    })();
-
-    for (let i = 0; i < pangram.length; i++) {
-        const cp = pangram[i].charCodeAt();
-
-        if (cp >= 97 && cp <= 122) {
-            alphabet[`${pangram[i]}`] = 1;
-        } else if (cp >= 97 - 32 && cp <= 122 - 32) {
-            alphabet[`${pangram[i].toLowerCase()}`] = 1;
-        }
-    }
-
-    for (const charExists of Object.values(alphabet)) {
-        if (!charExists) {
-            return false;
-        }
-    }
-
-    return true;
+    let s = pangram
+        .toLowerCase()
+        .replace(/[^a-z]/g, '')
+        .replace(/(.)(?=.*\1)/g, "")
+        .split('').sort().join('');
+    return (s === "abcdefghijklmnopqrstuvwxyz");
 };
